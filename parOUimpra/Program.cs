8a8ae1485f9fddc0c0d3c82b4c77ac1eb2d0e125
alvim.lopez@gmail.com <alvim.lopez@gmail.com>
2020-01-10 13:43:22 -0300

    acrescentei mais imformação

---
+++ parOUimpra/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parOUimpra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("entre com um numero : ");
+            int n = int.Parse(Console.ReadLine());
+           if ( n % 2 == 0)
+            {
+                Console.WriteLine("e par");
+            }
+          
+            else
+            {
+                Console.WriteLine("e impar");
+            }
+               
+                  
+            
+        }
+    }
+}
